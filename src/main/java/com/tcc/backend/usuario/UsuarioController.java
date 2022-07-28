ba3081723baa25babfd1328ae9eff62356ed06fe
mateusvfreitas <mateusvfreitas@gmail.com>
2022-07-28 20:20:27 -0300

    feat: criação CRUD de usuarios

--- src/main/java/com/tcc/backend/usuario/UsuarioController.java
+++ src/main/java/com/tcc/backend/usuario/UsuarioController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +34,46 @@ public class UsuarioController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-   
+
+    @PostMapping
+    public ResponseEntity<UsuarioDto> criar(@RequestBody UsuarioDto usuarioDto) {
+        try {
+            UsuarioDto usuarioRequest = usuarioService.criarUsuario(usuarioDto);
+            return new ResponseEntity<>(usuarioRequest, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDto> consultarPorId(@PathVariable("id") Long id) {
+        UsuarioDto usuarioRequest = usuarioService.consultarPorId(id);
+        if (usuarioRequest != null) {
+            return new ResponseEntity<>(usuarioRequest, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDto> atualizar(@PathVariable("id") Long id,
+            @RequestBody UsuarioDto usuarioDto) {
+        UsuarioDto usuarioRequest = usuarioService.atualizarUsuario(id, usuarioDto);
+        if (usuarioRequest != null) {
+            return new ResponseEntity<>(usuarioRequest, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletar(@PathVariable("id") Long id) {
+        try {
+            usuarioService.deletarUsuario(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+    
 }
--- src/main/java/com/tcc/backend/usuario/UsuarioService.java
+++ src/main/java/com/tcc/backend/usuario/UsuarioService.java
@@ -2,6 +2,7 @@ package com.tcc.backend.usuario;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -25,5 +26,33 @@ public class UsuarioService {
         return usuariosDto;
     }
 
+    public UsuarioDto criarUsuario(UsuarioDto usuarioDto){
+        Usuario usuario = usuarioRepository.save(modelMapper.map(usuarioDto, Usuario.class));
+
+        UsuarioDto usuarioResponse = modelMapper.map(usuario, UsuarioDto.class);
+
+        return usuarioResponse;
+    }
+
+    public UsuarioDto consultarPorId(Long id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        if(usuario.isPresent()){
+            return modelMapper.map(usuario.get(), UsuarioDto.class);
+        } else{
+            return null;
+        }
+    }
+
+    public UsuarioDto atualizarUsuario(Long id, UsuarioDto usuarioDto){
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow();
+        usuario.setIsAdmin(usuarioDto.getIsAdmin());
+        usuario.setNome(usuarioDto.getNome());
+        usuarioRepository.save(usuario);
+        return modelMapper.map(usuario, UsuarioDto.class);
+    }
     
+    public void deletarUsuario(Long id){
+        usuarioRepository.deleteById(id);
+    }
 }
